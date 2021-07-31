efa5f7df418658e3ad5144b2b42ce639aca4f302
Zike01 <zikekomakoma@gmail.com>
2021-07-31 20:46:53 +0300

    Add comments to index,js / Add 'copied' message.

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,27 @@
-function copyColor(index) {
-    const div = document.getElementById("color" + index);
-
+function copyColor(index_inner, index_outer) {
+    // Get the div element and it's background color
+    const div = document.getElementById("color" + index_inner);
     let bgColor = window.getComputedStyle(div).backgroundColor;
+
+    // Create a textarea element
     const text  = document.createElement('textarea');
     document.body.appendChild(text);
 
+    // Set the textarea value to the div background color
     text.value = bgColor;
+
+    // Select text
     text.select();
     text.setSelectionRange(0, 99999);
     
-
     document.execCommand('copy');
     document.body.removeChild(text);
+
+    var copied = document.getElementById("copied" + index_outer)
+    copied.innerHTML = "Copied " + bgColor + "!";
+    copied.style.color = "green";
+
+    window.setTimeout(function() {
+        copied.innerHTML = "";
+    }, 5000);
 }
